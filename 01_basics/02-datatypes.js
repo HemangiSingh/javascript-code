4f911940ffc9b3cc03460034b93395702d5f2d82
HemangiSingh <168717056+HemangiSingh@users.noreply.github.com>
2024-05-03 08:33:31 +0000

    datatype

---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // this will treat all JS code as newer version of JavaScript
+
+//alert(3+3)
+/* we can't write like that
+because we are using node not browser or html
+In node we can't perform arithmetic operations like that in alert
+there is another syntax for doing so*/
+
+/* sometimes we write ";" sometimes we don't
+but it's prefferable not to write ";" */
+
+/* In your code readability matters a lot
+so try to write code as readable and future proof as possible */
+
+console.log(3+3)
+
+console.log(3
+    +3) /* can write like that but not prefferable
+    because it decreases readability of code */
+    
+console.log(3+3); console("Hemangi")/* can write like that but not prefferable
+because it decreases readability of code */
